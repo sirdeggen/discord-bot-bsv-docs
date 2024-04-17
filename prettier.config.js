84e22d3f633654e0c75deef12679aba6949ee4b5
Deggen <d.kellenschwiler@bsvblockchain.org>
2024-04-17 16:19:57 -0500

    init

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "trailingComma": "es5",
+    "tabWidth": 4,
+    "semi": false,
+    "singleQuote": true,
+    "arrowParens": "avoid",
+    "printWidth": 120,
+    "endOfLine": "lf"
+}
